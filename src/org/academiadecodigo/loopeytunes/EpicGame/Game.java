31bab23331185a7fbcf83893aaca2fbe942c1d2e
Francisco <franciscosoaresp9@hotmail.com>
2021-10-16 03:48:26 +0100

    menu

--- src/org/academiadecodigo/loopeytunes/EpicGame/Game.java
+++ src/org/academiadecodigo/loopeytunes/EpicGame/Game.java
@@ -2,28 +2,22 @@ package org.academiadecodigo.loopeytunes.EpicGame;
 
 import org.academiadecodigo.loopeytunes.EpicGame.Factorys.FoodFactory;
 import org.academiadecodigo.loopeytunes.EpicGame.GameObjects.Character;
-import org.academiadecodigo.loopeytunes.EpicGame.GameObjects.CharactersType;
 import org.academiadecodigo.loopeytunes.EpicGame.GameObjects.Food;
 import org.academiadecodigo.simplegraphics.graphics.Text;
 import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
-public class Game {
+public class Game implements KeyboardHandler {
     private static final int DELAY = 2000;
-    private static final int FOOD_TOTAL = 1;
+    private static final int FOOD_TOTAL = 10;
     public static Food foodItem;
     private Character player1;
     private Character player2;
-    private Keyboard kbPlayer1;
-    private Keyboard kbPlayer2;
-    private KeyboardEvent aPressed;
-    private KeyboardEvent kPressed;
     private Text scorePlayer1;
     private Text scorePlayer2;
-
-    public Game() {
-    }
+    private boolean gameOn = false;
 
     public void init() {
         Field.generateField();
@@ -38,69 +32,67 @@ public class Game {
 
     public void startEngine() throws InterruptedException {
         commandsOn();
-
+        while (!gameOn) {
+            Thread.sleep(DELAY);
+        }
+        Field.HideStart();
         for (int i = 0; i < FOOD_TOTAL; i++) {
             playerAppear();
             scoreUpdate();
 
 
-            Thread.sleep(DELAY );
+            Thread.sleep(DELAY);
             foodItem = FoodFactory.makeFood();
-            Thread.sleep(DELAY/2);
+            Thread.sleep(DELAY / 2);
 
             foodItem.getPicture().delete();
-
 
         }
 
         if (player1.getScore() > player2.getScore()) {
             System.out.println("Player 1 wins");
             Field.playerOneVictory();
-            scoreAppear();
             return;
         }
+
         if (player1.getScore() == player2.getScore()) {
             System.out.println("It's a tie");
             Field.Draw();
-            scoreAppear();
             return;
         }
+
         System.out.println("Player 2 wins");
         Field.PlayerTwoVictory();
-        scoreAppear();
 
     }
 
     public void playerCreat() {
-        player1 = new Character(CharactersType.TAZ);
-        player2 = new Character(CharactersType.COYOTE);
+        player1 = new Character();
+        player2 = new Character();
     }
 
     public void playerAppear() {
-        player1.changePic(230, 350, "/Users/codecadet/Desktop/EpicGame/epicgame/Pics/Taz2.png");
-        player2.changePic(870, 350, "/Users/codecadet/Desktop/EpicGame/epicgame/Pics/coyote2.png");
+        player1.changePic(230, 350, "Pics/Taz2.png");
+        player2.changePic(870, 350, "Pics/coyote2.png");
     }
 
     public void commandsOn() {
-        kbPlayer1 = new Keyboard(player1);
-        aPressed = new KeyboardEvent();
+        Keyboard kbPlayer1 = new Keyboard(player1);
+        KeyboardEvent aPressed = new KeyboardEvent();
         setCommands(aPressed, KeyboardEvent.KEY_A);
 
-        kbPlayer2 = new Keyboard(player2);
-        kPressed = new KeyboardEvent();
+        Keyboard kbPlayer2 = new Keyboard(player2);
+        KeyboardEvent kPressed = new KeyboardEvent();
         setCommands(kPressed, KeyboardEvent.KEY_K);
 
         kbPlayer1.addEventListener(aPressed);
         kbPlayer2.addEventListener(kPressed);
 
-
     }
-
 
     public void setCommands(KeyboardEvent keyboardEventName, int keyboardEvent) {
         keyboardEventName.setKey(keyboardEvent);
         keyboardEventName.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
-
 
     }
 
@@ -111,6 +103,7 @@ public class Game {
         scorePlayer2.grow(40, 15);
         scorePlayer1.draw();
         scorePlayer2.draw();
+
     }
 
     public void scoreUpdate() {
@@ -118,5 +111,19 @@ public class Game {
         scorePlayer2.setText("Score: " + player2.getScore());
     }
 
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        if (keyboardEvent.getKey() == KeyboardEvent.KEY_SPACE) {
+            gameOn();
+        }
+    }
 
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
+
+    public void gameOn() {
+        gameOn = true;
+    }
 }
